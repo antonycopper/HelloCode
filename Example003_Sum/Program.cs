cc027c5073fd5dc1a6dd8fa478ce9e2a1007a9c4
Anton <antonycopper22@gmail.com>
2023-03-26 21:27:04 +0700

    003 програмс

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
-Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random().Next(1,10); // 1 2 3 4 ...9
+int numberB = new Random().Next(1, 10);
+int result = numberA + numberB;
+Console.WriteLine(result);
 
