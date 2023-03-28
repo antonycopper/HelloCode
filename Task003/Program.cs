a44dd959fad3ecd1717710ae926f75da169b0748
Anton <antonycopper22@gmail.com>
2023-03-29 00:43:45 +0700

    выполнили задание Task03

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -2,3 +2,33 @@
 //3 -> Среда
 //5 -> Пятница
 
+Console.WriteLine("Введите число, которому соответствует день недели: ");
+string day = Console.ReadLine();
+if (day == "1")
+{
+    Console.WriteLine("Сегодня понедельник");
+}
+else if (day == "2")
+{
+    Console.WriteLine("Вторник!");
+}
+else if (day == "3")
+{
+    Console.WriteLine("Среда))");
+}
+else if (day == "4")
+{
+    Console.WriteLine("Четверг :-0");
+}
+else if (day == "5")
+{
+    Console.WriteLine("Пятница :-p");
+}
+else if (day == "6")
+{
+    Console.WriteLine("Суббота ;-)");
+}
+else if (day == "7")
+{
+    Console.WriteLine("Воскресенье :-/");
+}
