5045f44bd1570376b06c99adb2e191f0ce5e6f2c
Anton <antonycopper22@gmail.com>
2023-04-02 22:58:06 +0700

    Task09 Programs - решили задачу методом (функциями)

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -7,9 +7,10 @@
 
 int numberRandom = new Random().Next(10, 100); // 99+1
 Console.WriteLine($"Случайное число из отрезка 10-99 => {numberRandom}");
-
-int firstDigit = numberRandom / 10;
-int secondDigit = numberRandom % 10;
+int numberRandom2 = new Random().Next(10, 100); // 99+1
+Console.WriteLine($"Случайное число из отрезка 10-99 => {numberRandom2}");
+//int firstDigit = numberRandom / 10;
+//int secondDigit = numberRandom % 10;
 
 /*if (firstDigit > secondDigit)
 {
@@ -20,6 +21,19 @@ else
     Console.WriteLine($"Наибольшая цифра числа => {secondDigit}");
 }
 */
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
- Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
+//int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+ //Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
 //Mat.Max(firsDigit, secondDigit);
+
+int maxDigit = MaxDigit (numberRandom);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
+int maxDigit2 = MaxDigit (numberRandom2);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit2}");
+
+int MaxDigit (int numRandom) //numRandom = numRandom
+{
+    int firstDigit = numRandom / 10;
+    int secondDigit = numRandom % 10;
+    int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;    
+    return maxDigit;
+}
