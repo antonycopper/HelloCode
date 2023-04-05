0a30060f636b459df2fd5eced39da2f8f53e83bc
Anton <antonycopper22@gmail.com>
2023-04-06 01:46:54 +0700

    Решили задачу task22

---
+++ Task22/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+
+string Spreadsheet(int nNum)
+{
+    string spreadsheetResult = "________\n";
+    for (int i = 1; i <= nNum; i++)
+    {
+        spreadsheetResult = spreadsheetResult + $"|{i,3}  | {i * i,3}| \n"; // выравниваем 3 знака
+    }
+    spreadsheetResult = spreadsheetResult+ "-----------";
+    return spreadsheetResult;
+}
+
+Console.Write("Введи  число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string result = Spreadsheet(n);
+Console.Write(result);
