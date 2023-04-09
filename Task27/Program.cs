8cf8d02cab02120727534e122f2f1fffbd0e6ff4
Anton <antonycopper22@gmail.com>
2023-04-09 16:17:10 +0700

    Выполнили ДЗ task25, task27, task29

---
+++ Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число
+и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumDigit(number);
+Console.WriteLine($"Сумма цифр в числе {number} равна {sum}");
+
+int SumDigit(int num)
+{
+    int sum = 0;
+
+    while (num != 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
---
+++ Task29/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+и выводит их на экран.
+
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 1, 33, 6]*/
+
+int[] array = new int[8];
+
+Fullness(array);
+
+PrintArray(array);
+Console.Write(" -> [");
+PrintArray(array);
+Console.Write("]");
+
+void Fullness(int[] array)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(1, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
