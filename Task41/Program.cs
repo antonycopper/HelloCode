2da3390ce65520a61d1baf2661f7fad85f56dca1
Anton <antonycopper22@gmail.com>
2023-04-22 23:50:12 +0700

    Выполнили ДЗ task41 и task 43

---
+++ Task41/Program.cs
@@ -0,0 +1,51 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+-1, -7, 567, 89, 223-> 3*/
+
+
+Console.Write("Введите длину массива: ");
+int arrSize = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArreyInput(arrSize);
+
+int countnum = CountPositivNum(arr);
+
+PrintArray(arr);
+
+Console.Write($" Колличество положительных чисел равно {countnum}");
+
+int[] CreateArreyInput(int length)
+{
+    int[] newArray = new int[length];
+
+    for (int i = 0; i < length; i++) // либо for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return newArray;
+}
+
+int CountPositivNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (arr[i] > 0) count += 1;
+    }
+    return count;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 43: Напишите программу, 
+которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+Console.WriteLine("Вводим значение для функции y = k1 * x + b1");
+Console.Write("b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Вводим значение для функции y = k2 * x + b2");
+Console.Write("b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+double[] result = FindCrossPoint(b1, k1, b2, k2);
+
+PrintArray(result);
+
+double[] FindCrossPoint(double b1, double k1, double b2, double k2)
+{
+    double[] res = new double[2];
+    res[0] = (b2 - b1) / (k1 - k2);
+    res[1] = k2 * res[0] + b2;
+    return res;
+}
+void PrintArray(double[] array)
+{
+    //Console.WriteLine(@"[{0}]", string.Join("; ", array));
+    Console.Write("[");
+    Console.Write(string.Join(", ", array.Select(x => Math.Round(x, 2))));
+    Console.Write("]");
+}
