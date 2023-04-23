d64c2c9faf9ef707a8533e248871e5d95fc0e746
Anton <antonycopper22@gmail.com>
2023-04-23 14:34:40 +0700

    Выполнили ДЗ task54,task56, task58, task60 и task62

---
+++ Task54/Program.cs
@@ -0,0 +1,59 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] arr = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max);
+        }
+    }
+    return arr;
+}
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+void SortMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] newMatrix = CreateMatrixRndInt(3, 7, 1, 10);
+Console.WriteLine("Исходный массив: ");
+PrintMatrix(newMatrix);
+SortMatrix(newMatrix);
+Console.WriteLine("Построчно отсортированный массив: ");
+PrintMatrix(newMatrix);
---
+++ Task62/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+int[,] array = new int[4, 4];
+int value = 1;
+int height = array.GetLength(0);
+int width = array.GetLength(1);
+
+int top = 0;
+int bottom = height - 1;
+int left = 0;
+int right = width - 1;
+
+while (true)
+{
+    // заполнение сверху слева направо
+    for (int i = left; i <= right; i++)
+    {
+        array[top, i] = value++;
+    }
+
+    top++;
+    if (top > bottom) break;
+
+    // заполнение справа сверху вниз
+    for (int i = top; i <= bottom; i++)
+    {
+        array[i, right] = value++;
+    }
+
+    right--;
+    if (right < left) break;
+
+    // заполнение снизу справа налево
+    for (int i = right; i >= left; i--)
+    {
+        array[bottom, i] = value++;
+    }
+
+    bottom--;
+    if (bottom < top) break;
+
+    // заполнение слева снизу вверх
+    for (int i = bottom; i >= top; i--)
+    {
+        array[i, left] = value++;
+    }
+
+    left++;
+    if (left > right) break;
+}
+
+// вывод массива
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < width; j++)
+    {
+        Console.Write("{0,2} ", array[i, j]);
+    }
+    Console.WriteLine();
+}
